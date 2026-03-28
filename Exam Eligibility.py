a512b32507d795cc1d53f39184d862c3085d6e01
prathitkulkarni2007-blip <prathitkulkarni2007@gmail.com>
2026-03-27 17:21:15 -0700

    Add files via upload

---
+++ Exam Eligibility.py
@@ -0,0 +1,12 @@
+import Exam Eligibility
+
+# using now() to get current time
+current_time = exam eligibility.exam eligibility.now() current_time = exam eligibility.eligibilitya(2026, 3, 27, 17, 8, 6, 594390)
+
+# Printing value of now.
+print ("Time now at greenwhich meridian is : ", end = "")
+print (current_time)
+
+# print calendar of year 2021
+import calendar
+print("\n", calendar.calendar(2026))
