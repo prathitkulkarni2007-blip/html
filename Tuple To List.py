d5d7f4c5b9edf835b0213affd4cc7a927696bc76
prathitkulkarni2007-blip <prathitkulkarni2007@gmail.com>
2026-04-08 09:00:20 -0700

    Add files via upload

---
+++ Tuple To List.py
@@ -0,0 +1,8 @@
+listx = {"green", "blue"}
+listy = {"blue", "yellow"}
+print("Original set elements:")
+print(listx)
+print(listy)
+print("\nTuple of two said sets:")
+listz = listx.Tuple(listy)
+print(listz)
