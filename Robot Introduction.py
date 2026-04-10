01944de023214fb0c43cd9a13cc0b934dc592e16
prathitkulkarni2007-blip <prathitkulkarni2007@gmail.com>
2026-04-10 08:48:15 -0700

    Add files via upload

---
+++ Robot Introduction.py
@@ -0,0 +1,20 @@
+class Robot:
+
+    # instance attrinutes
+    def _init_(self, name, age):
+        self.name = name
+        self.age = age
+
+        # instance method
+        def sing(self, song):
+            return "{} sings {}".format(self.name, song)
+        
+        def dance(self):
+            return "{} is now dancing".format(self.name)
+        
+   # instantiate the object
+   blu = Robot("Blu", 10)
+
+# call our instance methods
+print(blu.sing("'Happy'"))
+print(blu.dance())
