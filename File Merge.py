80ae1c768bc988562b90085b8182158de3d4b4d8
prathitkulkarni2007-blip <prathitkulkarni2007@gmail.com>
2026-04-26 17:32:47 -0700

    Add files via upload

---
+++ File Merge.py
@@ -0,0 +1,19 @@
+  # Program to merge two file into a third file
+
+
+  # Reading data from file1
+  with open('Codingal.txt') as fp:
+       data1 = fp.read()
+
+  # Reading date from file2
+  with open('sample_doc.txt') as fp:
+    data2 = fp.read()
+
+  # Merging 2 files
+  # To add the data of file2
+  # from next line
+  data1 += "\n"
+  data1 += data2
+  print("Merging two files....")
+  with open ('MergedFile.txt', 'w') as fp:
+       fp.write(data1)
