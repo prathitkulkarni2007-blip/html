f3fa473f54c934bcfabdb99749423e56cd389610
prathitkulkarni2007-blip <prathitkulkarni2007@gmail.com>
2026-07-22 17:03:44 -0700

    Add files via upload

--- Introduce Yourself.py
+++ Introduce Yourself.py
@@ -1,6 +1,6 @@
-def intro(name):
+   def intro(name):
 
-    print("Hello, Good morning! I am", name)
-  
-  user_name = input("Enter your name")
-   intro(user_name)
+       print("Hello, Good morning! I am", name)
+
+  user name = input("Enter your name")
+  intro(user_name)
