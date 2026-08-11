6a600ce71d5adb233c0dbd99b2b25783730ac4ef
prathitkulkarni2007-blip <prathitkulkarni2007@gmail.com>
2026-08-10 17:28:07 -0700

    Add files via upload

--- Denomination Calculator.py
+++ Denomination Calculator.py
@@ -1,62 +1,64 @@
-  from tkinter import *
-  from tkinter import messagebox
-  from PIL import Image, ImageTk
+ from tkinter import *
+ from tkinter import messagebox
+ from PIL import Image, ImageTk
 
-  # Setting up Main Window
-  root = Tk()
-  root.title('Denomination Counter')
-  root.configure(bg='light blue')
-  root.geometry('650x400')
-  
-  # Adding Image and Labels in the Main Window 
-  upload = Image.open("app_img.jpg")
-  # Resize the image using resize() method
-  upload = upload.resize((300, 300))
-  image = ImageTk.PhotoImage(upload)
-  label = Label(root, image=image, bg='light blue')
-  label.place(x=180, y=20)
-  
-  label1 = Label(root
-      text="Hey User! Welcome to Denomination Counter Application.",
-        bg='light blue')
-      label1.place(relx=0.5, y=340, anchor=CENTER)
-  
-  # Function to display a messagebox and proceed if OK is clicked
-  def msg():
-      MsgBox = messagebox.showinfo(
-      "Alert", "Do you want to calculate the denomination count?")
-      if MsgBox == 'ok':
+ # Setting up Main Window
+ root = Tk()
+ root.title('Denomination Counter')
+ root.configure(bg='light blue')\
+ root.geometry('650x400')
+
+ # Adding Image and Labels in the Main Window
+ upload = Image.open("app_img.jpg")
+ # Resize the image using resize() method
+ upload = upload.resize((300, 300))
+ image = ImageTk.PhotoImage(upload)
+ label = Label(root, image=image, bg='light blue')
+ label.place(x=180, y=20)
+
+ label1 = Label(root,
+     text="Hey User! Welcome to Denomination Counter Application.",
+     bg='light blue')
+ label1.place(relx=0.5, y=340, anchor=CENTER)
+
+ # Function to display a messagebox and proceed if OK is clicked
+ def msg():
+     MsgBox = messagebox.showinfo(
+         "Alert", "Do you want to calculate the denomination count?")
+     if MsgBox == 'ok':
          topwin()
 
   # Adding Buttons to the main window
   button1 = Button(root,
-      text="Let's get started!",
-      command=msg,
-      bg='brown',
-      fg='white')
-   button1.place(x=260, y=360)
-  
-   def topwin():
-       # Top Window
-       top = Toplevel(root)
-       top.title("Currency Denomination Counter")
-       top.configure(bg='grey')
-       top.geometry('600X400')
+                   text="Let's get  started!",
+                   command=msg,
+                   bg='brown',
+                   fg='white')
+  button1.place(x=260, y=360)
 
-       # Centering Widgets in the Top Window
-       label.place(x=230, y=50)
-       entry.place(x=200, y=80)
-       btn.place(x=240, y=120)
-       lbl.place(x=140, y=170)
+  # Function for opening new/top window
+  def topwin():
+      top = Toplevel()
+      top.title("Denominations Calculator")
+      top.configure(bg='light grey')
+      top.geometry('600x350+50+50')
 
-       l1.place(x=180, y=200)
-       l2.place(x=180, y=230)
-       l3.place(x=180, y=260)
+      label = Label(top, text="Enter total amount", bg='light grey')
+      entry = Entry(top)
+      lbl = Label(top, text="Here are number of notes for each denomination", bg='light grey')
 
-       t1.place(x=270, y=200)
-       t2.place(x=270, y=230)
-       t3.place(x=270, y=260)
+      l1 = Label(top, text="2000", bg='light grey')
+      l2 = Label(top, text="500", bg='light grey')
+      l3 = Label(top, text="100", bg='light grey')
 
-       top.mainloop()
+      t1 = Entry(top)
+      t2 = Entry(top)
+      t3 = Entry(top)
 
-    root.mainloop()
+      def calculator():
+          try:
+              global amount
+              amount = int(entry.get())
+              
+            
+            
