e3378c5f9ae753ab004b133a15778201cb383cf6
prathitkulkarni2007-blip <prathitkulkarni2007@gmail.com>
2026-08-28 17:11:39 -0700

    Add files via upload

--- Factorial.py
+++ Factorial.py
@@ -1,16 +1,16 @@
-  # Factorial of a number using recursion
-  def recur_factorial(n):
-     if n == 1:
-         return n
-     else:
-         return n*recur_factorial(n-1)
+ # 02-factorial.py
+ # Topic: Counting with Recursion, Factorial Using Recursion
+ 
+ def factorial(n):
+     if n == 0:
+        return 1
+    return n * factorial(n - 1)
 
-    num = int(input("Enter a number"))
+ input("Factorial - n! = n x (n-1) x ... x 1 base case: 0! = 1.  Press Enter ")
+ print("  4! =", factorial(4), "  = 4 x 3 x 2 x 1")
+ print("  5! =", factorial(5),"   = 5 x 4 x 3 x 2 x 1")
 
-    # check if the number is negative 
-    if num < 0:
-       print("Sorry, factorial does not exist for negative numbers")
-    elif num == 0:
-       print("The factorial of 0 is 1")
-    else:
-       print("The factorial of", num, "is", recur_factorial(num))
+ n = int(input("Enter a number (try 3 or 6): "))
+ guess = input("What is " + str(n) + "!? ")
+ input("n! = n x factorial(n-1)  base case: 0! = 1. Press Enter ")
+ print(" ", str(n) + "! =", factorial(n), "  your guess:", guess)
